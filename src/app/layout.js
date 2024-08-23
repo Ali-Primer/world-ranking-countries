68c85eca8e1f193ecf99fed8086b5204d0fd3daf
Ali Mousavi <mousavia465@gmail.com>
2024-08-23 21:24:48 +0330

    start project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +9,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="be-vietnam-pro">{children}</body>
     </html>
   );
 }
