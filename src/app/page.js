ddc1a2ca2ced5b635ffd87005c6a620033b3a48f
Ali Mousavi <mousavia465@gmail.com>
2024-08-31 16:28:44 +0330

    search view done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useState } from "react";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+
   return (
     <>
-      hi
+      <div className="bc_box">
+        <div className="background">
+          <div className="background_logo">
+            <img src="./logo.svg" alt="Logo" />
+          </div>
+        </div>
+        <div className="lowerBackground"></div>
+      </div>
+      <div className="main_page">
+        <div className="page_header">
+          <div className="header_found">
+            <p>Found 234 countries</p>
+          </div>
+          <div className="header_search">
+            <img src="./Search.svg"/>
+            <input 
+              className="searchBox"
+              placeholder="Search by Name, Region, Subregion"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
